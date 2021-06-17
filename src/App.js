3be5ae37deebddddee331c5c3b59cce469199111
yobany <yobanydesign@gmail.com>
2021-06-17 11:09:38 -0400

    Added search component.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
+import { useState } from 'react'
+import Search from './components/Search'
 import markData from './data/mark'
 
+import './App.css'
+
 const App = () => {
-  console.log(markData.chapters[0])
+  const [word, setWord] = useState('')
+
+  const searchWord = event => {
+    event.preventDefault()
+    console.log(markData.chapters[0], word)
+  }
+
+  const handleInput = event => {
+    let input = event.target.value
+    setWord(input)
+  }
+
   return (
-    <p>BQ</p>
+    <div id='main_div'>
+      <Search searchWord={searchWord} handleInput={handleInput} />
+      <p>{word}</p>
+    </div>
   )
 }
 
---
+++ src/components/Search.js
@@ -0,0 +1,10 @@
+const Search = props => {
+    return (
+        <form id='search_form' autoComplete='off'>
+            <input id='search_input' type='text' placeholder='search a word' onChange={props.handleInput}></input>
+            <button id='search_button' type='submit' onClick={props.searchWord}>Search</button>
+        </form>
+    )
+}
+
+export default Search
